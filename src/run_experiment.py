dde676625d4e9aa190ddfdc0f79e1d58410ce0c4
Derek Fox <derekefox@gmail.com>
2024-05-31 14:55:38 -0400

    Cleaned up a lot of stuff.

--- src/run_experiment.py
+++ src/run_experiment.py
@@ -1,37 +1,36 @@
 # -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
 """
 Created on Fri Mar  1 21:14:55 2019
 
-@author: gul15103
+@author: gul15103, defox
 """
 
 import svrg_ht
 import sgd_ht
-# import hsgd_ht
-# import gd_ht
 import scsg_ht
 
 import argparse
 
 parser = argparse.ArgumentParser(description='hardthresholding methods')
-parser.add_argument('--epoch', default=50, type=int, metavar='N', help='number of total epochs to run')
-parser.add_argument('--file_path',default='F:/Users/gul15103/Desktop/E2006.test',  type=str)
+parser.add_argument('--epoch', default=10, type=int, metavar='N', help='number of total epochs to run')
+parser.add_argument('--file_path', type=str)
 parser.add_argument('--output_folder', default='../logs/', type=str)
 parser.add_argument('--batchsize', default=1, type=int, metavar='N', help='mini-batch size (default: 128),only used for train')
 parser.add_argument('--htk', default=200, type=int)
 parser.add_argument('--B_type', default=1, type=int)
-parser.add_argument('--opt', default='sgd', type=str)
-parser.add_argument('--data_type', default='ridge', type=str)
+parser.add_argument('--opt', type=str)
+parser.add_argument('--data_type', type=str)
 parser.add_argument('--regularizer', default=1e-5, type=float)
 parser.add_argument('--etas', nargs='+', type=float)
 parser.add_argument('--B_types', nargs='+', type=int)
 
 args = parser.parse_args()
 
-#f= 'F:/Users/gul15103/Desktop/simulate_data1.npz' #1000_2500_0.5
-f= args.file_path#'F:/Users/gul15103/Desktop/mnist.scale'
-epoch =args.epoch
+f = args.file_path
+epoch = args.epoch
 batch_size = args.batchsize
+
 if args.data_type =='multi':
     loss = 'multi_class_softmax_regression'
     multi_class = True
@@ -44,7 +43,7 @@ elif  args.data_type =='ridge':
     
 batch_size_B_type='fixed'
 ht_k=args.htk
-log_interval=1
+log_interval=1000
 regularizer = args.regularizer
 output_folder = args.output_folder
 
@@ -54,9 +53,6 @@ etas = args.etas
 for eta  in etas:
   if args.opt == 'svrg':
       svrg_ht.svrg_ht( f,regularizer,  epoch, batch_size,  stepsize = eta, stepsize_type = "fixed", verbose = True, optgap = 10**(-30), loss = loss ,ht_k=ht_k, log_interval=log_interval,output_folder=output_folder, multi_class= multi_class)
-  elif args.opt == 'gd':
-    hsgd_ht.sgd_ht( f,regularizer,  epoch, batch_size,  stepsize = eta, stepsize_type = "fixed", verbose = True, optgap = 10**(-30), loss = loss , ht_k=ht_k,log_interval=log_interval, output_folder=output_folder , multi_class= multi_class)
-    #gd_ht.gd_ht( f,regularizer,  epoch, batch_size,  eta, stepsize_type = "fixed", verbose = True, optgap = 10**(-30), loss = loss , ht_k=ht_k ,log_interval=log_interval, output_folder=output_folder)
   elif args.opt == 'sgd':
     sgd_ht.sgd_ht( f,regularizer,  epoch, batch_size,  stepsize = eta, stepsize_type = "fixed", verbose = True, optgap = 10**(-30), loss = loss , ht_k=ht_k ,log_interval=log_interval, output_folder=output_folder, multi_class= multi_class)
   elif args.opt == 'scsg':
--- src/svrg_ht.py
+++ src/svrg_ht.py
@@ -3,200 +3,216 @@
 """
 Created on Wed May  9 13:24:52 2018
 
-@author: Neil
+@author: Neil, defox
 """
 
-#import sklearn
 import numpy as np
 
 import os
-#import copy
-#import time
-#from operator import add
 import time
 import Loss
 import Util
+import sys
 
-#import argparse
 from tensorboardX import SummaryWriter
 
-def svrg_ht( f, regularizer, epoch, batch_size , stepsize= 10**-5,  stepsize_type = "fixed", verbose = False, optgap = 10**(-3 ), loss = 'logistic',ht_k=100,log_interval=1,output_folder='../logs/', multi_class= False):
-  '''
-  INPUT:
-  x : data
-  y : vector for label 1 or 0
-  K : number of outest iterations
-  P : number of parallel precossors
-  batch_size4SVRG :
-  batch_size4H
-  stepsize  : default 10**-5
-  stepsize_type : fixed, decay 1/t, sqrt decay 1/sqrt( t )
-  OUTPUT:
-  '''
-  # global FILENAME
-  # FILENAME = outfile
 
-  # print FILENAME
-  # logging.basicConfig(level=logging.DEBUG, filename=FILENAME, filemode='w')
+def svrg_ht(
+    f,
+    regularizer,
+    epochs,
+    batch_size,
+    stepsize=10**-5,
+    stepsize_type="fixed",
+    verbose=False,
+    optgap=10 ** (-3),
+    loss="logistic",
+    ht_k=100,
+    log_interval=1000,
+    output_folder="../logs/",
+    multi_class=False,
+):
+    """
+    f: location of data file
+    regularizer: default val 1e-5
+    epochs: default val 10
+    batch_size: default val 1
+    stepsize: default val 1e-5
+    stepsize_type: fixed, decay, sqrtdecay, squaredecay - default val "fixed"
+    verbose: default val False
+    optgap: default val 10**(-3)
+    loss: regression type - logistic, svm, ridge, multi_class_softmax_regression - default val logistic
+    ht_k: sparsity - default val 100
+    log_interval: default val 1000
+    output_folder: default val '../logs/'
+    multi_class: default val False
+    """
 
-  if 'simulate' in f:
-      data = np.load( f )
-      x= data['x']
-      y= data['y']
-  else:
-      x, y = Util.readlibsvm(f)
-  if 'news20' in f:
-      y = y -1  #x = sklearn.preprocessing.normalize( x )
-  index = np.arange(np.shape(x)[0])
-  np.random.shuffle(index)
-  x= x[index, :]
-  y = y[index ]
-  #label = (label == 1 )* 1
-  folder_name = os.path.basename(f) + '_loss_' + str(loss)  +'_opt_svrg_'+  'stepsize_'+str( stepsize)+'_batchsize_'+ str(batch_size)+'_ht_k_'+ str(ht_k)+'_log_interval_'+ str(log_interval)+'_epoch_'+ str(epoch)
-  logger = SummaryWriter(output_folder+folder_name)
-  file = open(output_folder+folder_name+str(".txt"),"w")
-  
-  file.write( "The dataset : " + str( f) + '\n' )
-  file.write( "The number of instances N : " + str( x.shape[0])  + '\n')
-  file.write( "The number of features p : " + str( x.shape[1]) + '\n' )
-  file.write( "The step size : " + str(  stepsize )  + '\n')
-  file.write( "The epoch : " + str(  epoch )  + '\n')
-  file.write( "The loss type: " + str(loss) )
-  file.write( "The regularizer : " + str(  regularizer )  + '\n')
-  file.write( "The ht_k : " + str(  ht_k )  + '\n')
-  file.write( "The batch_size : " + str(  batch_size ) + '\n' )
+    if "simulate" in f:
+        data = np.load(f)
+        x = data["x"]
+        y = data["y"]
+    else:
+        x, y = Util.readlibsvm(f)
+    if "news20" in f:
+        y = y - 1
 
-  file.write('num_epoch,num_IFO,num_HT,loss\n')     
-  
-  if loss=='logistic':
-    loss = Loss.LogisticLoss_version2()
-  elif loss == 'svm' :
-    loss = Loss.svm_quadratic()
-  elif loss == 'ridge' :
-    loss = Loss.ridge_regression()
-  elif loss == 'multi_class_softmax_regression':
-    loss = Loss.multi_class_softmax_regression()
-   #regularizer = 1/x.shape[0]
+    index = np.arange(np.shape(x)[0])
+    np.random.shuffle(index)
+    x = x[index, :]
+    y = y[index]
 
-  #loss = Loss.ridge_regression()
-  print( "The dataset : " + str( f) )
-  print( "The number of instances N : " + str( x.shape[0]) )
-  print( "The number of features p : " + str( x.shape[1]) )
-  print( "The batch size for SGD : " + str( batch_size ) )
-  print( "The step size : " + str(  stepsize ) )
-  print( "The epoch : " + str(  epoch ) )
-  print( "The loss type: " + str(loss) )
-  print( "The regularizer : " + str(  regularizer ) )
-  
-  
-  if multi_class:
-      w  = np.zeros((x.shape[1],len( np.unique( y ))))
-  else: 
-      w = np.zeros(x.shape[1],)
-   
-  # ---------------------------------------------
- 
-  #store information
-  obj_list  = list();
-  t0 = time.time()
-  iteration=0
-  
-  for k in range (int(epoch/2)):
-    #print("master iteration ", k)
-      #setup stepsize
+    output_folder_suffix = f"{os.path.basename(f)}_loss_{loss}_opt_svrg_stepsize_{stepsize}_batchsize_{batch_size}_ht_k_{ht_k}_log_interval_{log_interval}_epoch_{epochs}"
+    output_folder_name = output_folder + output_folder_suffix
+    logger = SummaryWriter(output_folder_name)
+    file = open(output_folder_name + ".txt", "w")
+
+    file.write(f"The dataset: {f}\n")
+    file.write(f"The number of instances N: {x.shape[0]}\n")
+    file.write(f"The number of features p: {x.shape[1]}\n")
+    file.write(f"The step size: {stepsize}\n")
+    file.write(f"The epoch: {epochs}\n")
+    file.write(f"The loss type: {loss}")
+    file.write(f"The regularizer: {regularizer}\n")
+    file.write(f"The ht_k: {ht_k}\n")
+    file.write(f"The batch_size: {batch_size}\n")
+
+    file.write("num_epoch,num_IFO,num_HT,loss\n")
+
+    if loss == "logistic":
+        loss = Loss.LogisticLoss_version2()
+    elif loss == "svm":
+        loss = Loss.svm_quadratic()
+    elif loss == "ridge":
+        loss = Loss.ridge_regression()
+    elif loss == "multi_class_softmax_regression":
+        loss = Loss.multi_class_softmax_regression()
+
+    print(f"The dataset : {f}")
+    print(f"The number of instances N : {x.shape[0]}")
+    print(f"The number of features p : {x.shape[1]}")
+    print(f"The batch size for SGD : {batch_size}")
+    print(f"The step size : {stepsize}")
+    print(f"The epoch : {epochs}")
+    print(f"The loss type: {loss}")
+    print(f"The regularizer : {regularizer}")
+
+    if multi_class:
+        w = np.zeros((x.shape[1], len(np.unique(y))))
+    else:
+        w = np.zeros(x.shape[1])
+
+    obj_list = []
+    t0 = time.time()
+    iter = 0
+
+    for epoch in range(epochs):
+        # print("master iteration ", k)
+
+        eta = updateStepsize(stepsize, stepsize_type, epoch)
+
+        w_multi = np.copy(w)
+        mu = loss.grad(x, y, w, regularizer)
+
+        for batch in range(int(x.shape[0] / batch_size)):
+
+            # print("master iteration ", k, t)
+            if epoch < 1:
+                # lock.acquire()
+                sample_id = np.random.randint(x.shape[0] - batch_size, size=1)
+                sample_id = sample_id[0]
+                g1 = loss.grad(
+                    x[sample_id : sample_id + batch_size],
+                    y[sample_id : sample_id + batch_size],
+                    w,
+                    regularizer,
+                )
+                # print(multiprocessing.current_process(), "Grad w ", w[ 0:3])
+                # lock.release()
+                v = g1
+            else:
+                # lock.acquire()
+                sample_id = np.random.randint(x.shape[0] - batch_size, size=1)
+                sample_id = sample_id[0]
+                g1 = loss.grad(
+                    x[sample_id : sample_id + batch_size],
+                    y[sample_id : sample_id + batch_size],
+                    w,
+                    regularizer,
+                )
+                # print(multiprocessing.current_process(), "Grad w ", w[ 0:3])
+                # lock.release()
+                g2 = loss.grad(
+                    x[sample_id : sample_id + batch_size],
+                    y[sample_id : sample_id + batch_size],
+                    w_multi,
+                    regularizer,
+                )
+                v = g1 - g2 + mu
+
+            # print(multiprocessing.current_process(), "Before Update w ", w[0:3], w[ -3:-1])
+            w = w + eta * -v
+            if multi_class:
+                for j in range(w.shape[1]):
+                    w[np.absolute(w[:, j]).argsort()[:-ht_k][::-1], j] = 0
+            else:
+                w[np.absolute(w).argsort()[:-ht_k][::-1]] = 0
+                # print(multiprocessing.current_process(), "After Update w ", w[0:3], w[ -3:-1])
+            iter = iter + 1
+
+            if (batch + 1) % log_interval == 0:
+                obj_temp = loss.obj(x, y, w, regularizer)
+                time_k = time.time()
+                print("------------------------------------------------")
+                if verbose:
+                    print(f"Epoch: {epoch+1}, data passes: {epoch+1}, time: {time_k - t0}, stepsize: {eta}")
+                    print(f"Norm for w : {np.square(np.linalg.norm(w))}")
+                print(f"Iteration {iter}: objective value = {obj_temp}")  # always print this
+                obj_list.append(obj_temp)
+
+                if epoch > 0 and np.abs(obj_list[-1] - obj_list[-2]) < optgap:
+                    print(f"Optimality gap tolerance reached: {optgap}")
+                    break
+                logger.add_scalar("loss_number-IFO", obj_temp, (2 * epoch) * x.shape[0] + 2 * batch * batch_size,)
+                logger.add_scalar("loss_number-HT", obj_temp, iter)
+                file.write(f"{((2 * epoch) * x.shape[0] + 2 * batch * batch_size) / x.shape[0]},{iter},{obj_temp}\n")
+    file.close()
+
+
+def updateStepsize(stepsize, stepsize_type, epoch):
     if stepsize_type == "fixed":
-      eta = stepsize
-    elif stepsize_type == "decay":
-      eta = 1./( k+1) 
+        return stepsize
+
+    if stepsize_type == "decay":
+        eta = stepsize * 1.0 / (epoch + 1)
     elif stepsize_type == "sqrtdecay":
-    #eta = 1/np.sqrt(k*L + t +1 )
-      eta = stepsize*1./np.sqrt( k +1)
+        eta = stepsize * 1.0 / np.sqrt(epoch + 1)
     elif stepsize_type == "squaredecay":
-      eta = ( 1/np.square(k + 1 ))
-      
-    w_multi = np.copy(w)
-    u = loss.grad(x, y, w, regularizer)
-    
-    for t in range(int(x.shape[0]/batch_size)):
-
-      #print("master iteration ", k, t)
-      if k <1:
-        #lock.acquire()
-        sample_id = np.random.randint(x.shape[0]-batch_size, size=1)
-        sample_id = sample_id[0]
-        g1 = loss.grad(x[sample_id:sample_id+batch_size], y[sample_id:sample_id+batch_size], w, regularizer)
-        #print(multiprocessing.current_process(), "Grad w ", w[ 0:3])
-        #lock.release()
-        v = g1
-      else:
-        #lock.acquire()
-        sample_id = np.random.randint(x.shape[0]-batch_size, size=1)
-        sample_id = sample_id[0]
-        g1 = loss.grad(x[sample_id:sample_id+batch_size], y[sample_id:sample_id+batch_size], w, regularizer)
-        #print(multiprocessing.current_process(), "Grad w ", w[ 0:3])
-        #lock.release()
-        g2 = loss.grad(x[sample_id:sample_id+batch_size], y[sample_id:sample_id+batch_size], w_multi, regularizer )
-        v = g1 - g2 + u
-        
-        #print( 'w_multi : ', np.square( np.linalg.norm( w_multi )))
-      
-      Hv= -v
-      #print( 'w : ', np.square( np.linalg.norm( w )))
-      #print(multiprocessing.current_process(), "Before Update w ", w[0:3], w[ -3:-1])
-      w = w + eta*Hv
-      if multi_class:
-          for j in range( w.shape[1]):
-              w[ np.absolute(w[:,j]).argsort()[:-ht_k][::-1], j ] = 0
-      else:
-          w[ np.absolute(w).argsort()[:-ht_k][::-1]] = 0
-         #print(multiprocessing.current_process(), "After Update w ", w[0:3], w[ -3:-1])
-      iteration = iteration + 1
-     
-    #--------------------------------------
-
-      if (t+1) % log_interval == 0:
-          #obj_temp = loss.obj( x, y, w, regularizer )/loss.obj( x, y, np.zeros(x.shape[1],), regularizer )
-          obj_temp = loss.obj( x, y, w, regularizer )
-          #if k>0:
-          time_k = time.time()
-          #print( 'Epoch: '+ str(k+1) +', data passes : '+ str((k+1))+ ', time :' + str(time_k - t0 ))
-            #store informations
-            #if verbose:
-          #print(  "objective value  %.30f" % obj_temp)
-          #print( "Norm for w : ", np.square( np.linalg.norm( w )))
-          obj_list.append(obj_temp)
-          if k>0 and np.abs( obj_list[-1] - obj_list[ -2 ] ) < optgap:
-            print( "Optimality gap tolerance reached : optgap " + str( optgap ))
-            break
-              #return obj_list, datapasses_list, time_list
-    
-          print( 'loss ' + str( obj_temp))     
-          logger.add_scalar('loss_number-IFO', obj_temp, (2*k)*x.shape[0] + 2*t*batch_size )
-          logger.add_scalar('loss_number-HT', obj_temp, iteration)
-          file.write(str( ((2*k)*x.shape[0] + 2*t*batch_size)/x.shape[0])+','+str(iteration)+','+ str(obj_temp)+'\n')  
-  file.close()
-  
-def main(batch_size = 1, eta=0.01, epoch = 10):
-  #f = sys.argv[1]
-  #dataset = 'real-sim'
-  f= 'F:/Users/gul15103/Desktop/simulate_data1000_2500_0.5.npz'
-  #f = '/Users/Neil/Desktop/E2006.train'
-  #f = '../data/' + dataset
-  #f= '../data/real-sim'
-  
-  
-
-  
-  f= 'F:/Users/gul15103/Desktop/mnist.scale'
-  #eta=0.1
-  
-  loss = 'multi_class_softmax_regression'
-  regularizer=0.001
-  
-  #for eta in [0.5, 0.1, 0.01, 0.05] :
-  svrg_ht( f, regularizer, epoch, batch_size,  stepsize= eta, stepsize_type = "fixed", verbose = True, optgap = 10**(-30), loss = loss ,ht_k=200, multi_class= True)
-  
+        eta = stepsize * (1.0 / np.square(epoch + 1))
+    return eta
 
 
-if __name__== "__main__":
-  main()
+def main():
+    if len(sys.argv) < 3:
+        print("Usage: python svrg_ht.py <dataset_name> <loss_function>")
+    f = "../data/" + sys.argv[1]
+    loss = sys.argv[2]
+    multi_class = loss == "multiclass"
+
+    svrg_ht(
+        f,
+        regularizer=1e-5,
+        epochs=10,
+        batch_size=1,
+        stepsize=1,
+        stepsize_type="fixed",
+        verbose=True,
+        optgap=10 ** (-3),
+        loss=loss,
+        ht_k=1000,
+        multi_class=multi_class,
+    )
+
+
+if __name__ == "__main__":
+    main()
